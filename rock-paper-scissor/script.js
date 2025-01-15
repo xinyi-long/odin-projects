062b0641d03d871fc11852bd399ec73e5488f160
Xinyi Long <emmaxlong97@gmail.com>
2025-01-15 13:41:26 -0800

    update the rps-ui branch with UI updates

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -13,22 +13,70 @@ function getHumanChoice() {
     return user;
 }   
 
+function gameOver() {
+    buttons.forEach( (button) => {
+        button.remove();
+        // body.appendChild(finalMsg);
+        container.appendChild(restartBtn);
+    });
+}
+
+function restart() {
+    humanScore = 0;
+    computerScore = 0;
+    roundCounter = 1;
+
+    resultsDiv.innerText = 'Press button to start the game';
+    restartBtn.remove();
+
+    container.insertBefore( rockBtn, resultsDiv);
+    container.insertBefore( paperBtn, resultsDiv);
+    container.insertBefore( scissorBtn, resultsDiv);
+    
+}
+
 function playRound(humanchoice, computerchoice) {
     // let computerchoice = getComputerChoice();
     // let humanchoice = getHumanChoice();
+    resultsDiv.innerText = `Round ${roundCounter} 
+    User chooses: ${humanchoice} 
+    Computer chooses: ${computerchoice}`;
+
     if (computerchoice == humanchoice){
-        console.log("It's a draw!");
+        resultsDiv.innerText = `Draw! Your weapon ${humanchoice} is the same as ${computerchoice}
+        Score Table: \nYour score:${humanScore}\nComputer score:${computerScore}`;
     }
     else if (computerchoice == "rock" && humanchoice == "paper" || 
         computerchoice == "paper" && humanchoice == "scissors" ||
         computerchoice == "scissors" && humanchoice == "rock"
     ){
-        console.log(`Your weapon ${humanchoice} defeats ${computerchoice}. You win : D`);
+        humanScore++;
+        resultsDiv.innerText = `Your weapon ${humanchoice} defeats ${computerchoice}. You win : D
+        Score Table: \nYour score:${humanScore}\nComputer score:${computerScore}`;
     }
     else if (humanchoice == "rock" && computerchoice == "paper" || 
         humanchoice == "paper" && computerchoice == "scissors" ||
         humanchoice == "scissors" && computerchoice == "rock"){
-        console.log(`Your weapon ${humanchoice} is defeated by ${computerchoice}. You lose : (`);
+        computerScore++;
+        resultsDiv.innerText = `Your weapon ${humanchoice} is defeated by ${computerchoice}. You lose : (
+        Score Table: \nYour score:${humanScore}\nComputer score:${computerScore}`;
+    }
+    roundCounter++;
+    if (humanScore == 5 || computerScore == 5){
+        if ( humanScore === computerScore ) {
+            resultsDiv.innerText = `Draw!
+            Game last ${roundCounter} rounds. 
+            Scores table: \nUser: ${humanScore} \nComputer: ${computerScore}`;
+        } else if (humanScore > computerScore) {
+            resultsDiv.innerText = `User won the game!
+            Game last ${roundCounter} rounds. 
+            Scores table: \nUser: ${humanScore} \nComputer: ${computerScore}`;
+        } else {
+            resultsDiv.innerText = `Computer won the game!
+            Game last ${roundCounter} rounds. 
+            Scores table: \nUser: ${humanScore} \nComputer: ${computerScore}`;
+        }
+        return gameOver();
     }
 }
 
@@ -38,8 +86,40 @@ function playGame() {
     for (i = 0; i < 5; i++) {
         playRound(getHumanChoice(),getComputerChoice());
     }
-}
+}   
 
-// console.log(getComputerChoice());
-// console.log(getHumanChoice());
-playGame();
+// const buttons = document.querySelectorAll("button");
+
+// // we use the .forEach method to iterate through each button
+// buttons.forEach((button) => {
+//   // and for each one we add a 'click' listener
+//   button.addEventListener("click", () => {
+//     playRound(getHumanChoice(),getComputerChoice());
+//   });
+// });
+
+let humanScore = 0;
+let computerScore = 0;
+let roundCounter = 1;
+
+const container = document.querySelector("#container");
+const rockBtn = document.querySelector("#rockbtn");
+const paperBtn = document.querySelector("#paperbtn");
+const scissorBtn = document.querySelector("#scissorbtn");
+const resultsDiv = document.createElement("div");
+const buttons = document.querySelectorAll("button");
+const restartBtn = document.createElement('button');
+
+rockBtn.setAttribute('value', "rock");
+paperBtn.setAttribute('value',"paper");
+scissorBtn.setAttribute('value',"scissors");
+resultsDiv.innerText = "Press the buttons to start the game.";
+container.appendChild(resultsDiv);
+restartBtn.innerText = 'Restart';
+
+
+rockBtn.addEventListener('click', () => playRound(rockBtn.value, getComputerChoice() ) );
+paperBtn.addEventListener('click', () => playRound(paperBtn.value, getComputerChoice() ) );
+scissorBtn.addEventListener('click', () => playRound(scissorBtn.value, getComputerChoice() ) );
+restartBtn.addEventListener('click', restart);
+  
